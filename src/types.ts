d7132e2b20be658c422171f18beb38206ee0d992
rjbma <ricardo.araujo@obconnect.io>
2021-03-30 23:04:01 +0100

    type changes

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,51 @@
-../svelte/src/types.ts
+type OperationMode = 'static' | 'tail'
+
+/**A log message has sent by the server to the clients */
+type LogMessage = {
+  seq: number
+  data: Record<string, any>
+  index: string[]
+}
+
+/**A log message that's already been formatted and ready to be displayed */
+type FormattedMessage = {
+  seq: number
+  rawMessage: Record<string, any>
+  formattedMessage: Record<string, any>
+}
+
+/**A function that formats a log message */
+type LogFormatter = Record<string, LogColumnFormatter>
+type LogColumnFormatter = (message: LogMessage['data'], seq: number) => any
+
+// ***
+// SERVER MESSAGES
+// ***
+
+// the message sent to clients that either just connected or changed operation model (tail <-> static)
+type InitMessage = {
+  type: 'init'
+  mode: OperationMode
+  size: number
+  offsetSeq: number
+  window: LogMessage[]
+}
+// the message sent to all clients once a new message comes in from stdin
+type UpdateMessage = { type: 'update'; size: number; message: LogMessage }
+// all messages in the direction Server -> Client
+type ServerMessage = InitMessage | UpdateMessage
+
+// CLIENT MESSAGES
+type StaticClientMessage = { mode: 'static'; filter: string; offsetSeq: number }
+type TailClientMessage = { mode: 'tail'; filter: string }
+/**Messages sent from the client to the server, basically to signal it want to swith operation mode */
+type ClientMessage = StaticClientMessage | TailClientMessage
+
+export type {
+  LogMessage,
+  FormattedMessage,
+  LogFormatter,
+  LogColumnFormatter,
+  ServerMessage,
+  ClientMessage,
+}
